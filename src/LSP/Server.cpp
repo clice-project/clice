f0641eb4cf415aa1f242c5b62428e32d374eb2ef
ykiko <ykikoykikoykiko@gmail.com>
2024-06-30 18:46:25 +0800

    basic usage of libuv.

--- src/LSP/Server.cpp
+++ src/LSP/Server.cpp
@@ -1,11 +1,3 @@
-#include <uv.h>
 #include <LSP/Server.h>
 
-namespace clice {
-
-int Server::run() { 
-    uv_loop_t* loop = uv_default_loop();
-    return 0; 
-}
-
-} // namespace clice
+namespace clice {} // namespace clice
---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <LSP/Server.h>
+
+int main() {
+  clice::Server server;
+  server.run();
+  return 0;
+}
