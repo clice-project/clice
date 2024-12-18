6c627bc8ef18312471a69f110d6498d54af151d0
Shiyu <im.shiyu1225@gmail.com>
2024-12-18 18:34:20 +0800

    Impl DocumentSymbol.

--- src/Feature/DocumentSymbol.cpp
+++ src/Feature/DocumentSymbol.cpp
@@ -1,5 +1,4 @@
 #include "Feature/DocumentSymbol.h"
-#include "clang/Basic/SourceLocation.h"
 
 namespace clice {
 
@@ -33,15 +32,15 @@ struct LspProtoAdaptor {
     }
 };
 
+/// Clangd's DocumentSymbol Implementation:
+/// https://github.com/llvm/llvm-project/blob/main/clang-tools-extra/clangd/FindSymbols.cpp#L286
+
 /// Use DFS to traverse the AST and collect document symbols.
 struct DocumentSymbolCollector :
     public clang::RecursiveASTVisitor<DocumentSymbolCollector>,
     public LspProtoAdaptor {
 
     using Base = clang::RecursiveASTVisitor<DocumentSymbolCollector>;
-
-    /// Token buffer of given AST.
-    // clang::syntax::TokenBuffer* tkbuf;
 
     /// DFS state stack.
     std::vector<proto::DocumentSymbol> stack;
@@ -69,18 +68,40 @@ struct DocumentSymbolCollector :
     /// Collect a leaf node as the DocumentSymbol.
     void collect(proto::DocumentSymbol symbol) {
         if(stack.empty())
-            stack.push_back(std::move(symbol));
+            result.push_back(std::move(symbol));
         else
             stack.back().children.push_back(std::move(symbol));
+    }
+
+    /// Mark the symbol as deprecated.
+    void markDeprecated(proto::DocumentSymbol& symbol) {
+        symbol.tags.push_back(proto::SymbolTag{proto::SymbolTag::Deprecated});
+        symbol.deprecated = true;
     }
 
     bool TraverseDecl(clang::Decl* decl) {
         if(!decl)
             return true;
 
-        if(llvm::isa<clang::NamedDecl>(decl)) {
-            auto* named = llvm::dyn_cast<clang::NamedDecl>(decl);
-            if(notInMainFile(named->getLocation()))
+        if(llvm::isa<clang::TranslationUnitDecl>(decl))
+            return Base::TraverseDecl(decl);
+
+        if(!llvm::isa<clang::NamedDecl>(decl))
+            return true;
+
+        // auto* named = llvm::dyn_cast<clang::NamedDecl>(decl);
+        // auto name = named->getName();
+
+        if(notInMainFile(decl->getLocation()) || decl->isImplicit())
+            return true;
+
+        if(auto* ctsd = llvm::dyn_cast<clang::ClassTemplateSpecializationDecl>(decl)) {
+            if(!ctsd->isExplicitInstantiationOrSpecialization())
+                return true;
+        }
+
+        if(auto* vtsd = llvm::dyn_cast<clang::VarTemplateSpecializationDecl>(decl)) {
+            if(!vtsd->isExplicitInstantiationOrSpecialization())
                 return true;
         }
 
@@ -104,6 +125,163 @@ struct DocumentSymbolCollector :
 
         return res;
     }
+
+    bool TraverseEnumDecl(clang::EnumDecl* decl) {
+        auto range = toLspRange(decl->getSourceRange());
+        proto::DocumentSymbol symbol{
+            .name = decl->getNameAsString(),
+            .kind = proto::SymbolKind::Enum,
+            .range = range,
+            .selectionRange = range,
+        };
+
+        entry(std::move(symbol));
+        bool res = Base::TraverseEnumDecl(decl);
+        leave();
+
+        return res;
+    }
+
+    bool VisitEnumDecl(const clang::EnumDecl* decl) {
+        for(auto* enumerator: decl->enumerators()) {
+            auto range = toLspRange(enumerator->getSourceRange());
+            proto::DocumentSymbol symbol{
+                .name = enumerator->getNameAsString(),
+                .kind = proto::SymbolKind::EnumMember,
+                .range = range,
+                .selectionRange = range,
+            };
+
+            // Show the initializer value as the detail.
+            llvm::SmallString<32> sstr;
+            sstr.append("= ");
+            enumerator->getInitVal().toString(sstr);
+            if(sstr.size() > 10)
+                symbol.detail = "<initializer>";
+            else
+                symbol.detail = sstr.str().slice(0, sstr.size());
+
+            if(enumerator->isDeprecated())
+                markDeprecated(symbol);
+
+            collect(std::move(symbol));
+        }
+
+        return true;
+    }
+
+    bool TraverseCXXRecordDecl(clang::CXXRecordDecl* decl) {
+        constexpr auto Default = "<anonymous struct>";
+
+        auto range = toLspRange(decl->getSourceRange());
+        proto::DocumentSymbol symbol{
+            .range = range,
+            .selectionRange = range,
+        };
+        symbol.kind = decl->isAbstract() ? proto::SymbolKind::Interface
+                      : decl->isClass()  ? proto::SymbolKind::Class
+                                         : proto::SymbolKind::Struct;
+
+        if(auto name = decl->getName(); !name.empty())
+            symbol.name = name;
+        else
+            symbol.name = Default;
+
+        entry(std::move(symbol));
+        bool res = Base::TraverseCXXRecordDecl(decl);
+        leave();
+        return res;
+    }
+
+    bool VisitFieldDecl(const clang::FieldDecl* decl) {
+        auto range = toLspRange(decl->getSourceRange());
+        proto::DocumentSymbol symbol{
+            .name = decl->getNameAsString(),
+            .kind = proto::SymbolKind::Field,
+            .range = range,
+            .selectionRange = range,
+        };
+        symbol.detail = decl->getType().getAsString();
+
+        if(decl->isDeprecated())
+            markDeprecated(symbol);
+
+        collect(std::move(symbol));
+        return true;
+    }
+
+    std::string composeFuncSignature(const clang::FunctionDecl* decl) {
+        std::string signature = decl->getReturnType().getAsString();
+
+        signature += " (";
+        for(auto* param: decl->parameters()) {
+            signature += param->getType().getAsString();
+            signature += ",";
+        }
+        if(!decl->param_empty())
+            signature.pop_back();
+        signature += ")";
+
+        return signature;
+    }
+
+    bool TraverseFunctionDecl(clang::FunctionDecl* decl) {
+        if(!decl)
+            return true;
+
+        auto name = decl->getName();
+        if(auto spec = decl->getTemplateSpecializationInfo();
+           spec && !spec->isExplicitSpecialization()) {
+            return true;
+        }
+
+        return Base::TraverseFunctionDecl(decl);
+    }
+
+    bool VisitFunctionDecl(const clang::FunctionDecl* decl) {
+        auto range = toLspRange(decl->getSourceRange());
+        proto::DocumentSymbol symbol{
+            .name = decl->getNameAsString(),
+            .kind = proto::SymbolKind::Function,
+            .range = range,
+            .selectionRange = range,
+        };
+        symbol.detail = composeFuncSignature(decl);
+
+        if(decl->isDeprecated())
+            markDeprecated(symbol);
+
+        collect(std::move(symbol));
+        return true;
+    }
+
+    bool TraverseParmVarDecl(clang::ParmVarDecl* decl) {
+        return true;
+    }
+
+    bool TraverseVarDecl(clang::VarDecl* decl) {
+        if(!decl || decl->isLocalVarDeclOrParm() || decl->isTemplated())
+            return true;
+
+        return Base::TraverseVarDecl(decl);
+    }
+
+    bool VisitVarDecl(const clang::VarDecl* decl) {
+        auto range = toLspRange(decl->getSourceRange());
+        proto::DocumentSymbol symbol{
+            .name = decl->getNameAsString(),
+            .detail = decl->getType().getAsString(),
+            .kind = decl->isConstexpr() ? proto::SymbolKind::Constant : proto::SymbolKind::Variable,
+            .range = range,
+            .selectionRange = range,
+        };
+
+        if(decl->isDeprecated())
+            markDeprecated(symbol);
+
+        collect(std::move(symbol));
+        return true;
+    }
 };
 
 }  // namespace
@@ -116,10 +294,8 @@ json::Value documentSymbolCapability(json::Value clientCapabilities) {
 }
 
 proto::DocumentSymbolResult documentSymbol(proto::DocumentSymbolParams params, ASTInfo& ast) {
-
     DocumentSymbolCollector collector;
     collector.src = &ast.srcMgr();
-
     collector.TraverseDecl(ast.tu());
     return std::move(collector.result);
 }
--- unittests/Feature/DocumentSymbol.cpp
+++ unittests/Feature/DocumentSymbol.cpp
@@ -2,7 +2,6 @@
 #include <Feature/DocumentSymbol.h>
 
 #include "../Test.h"
-#include "Support/Enum.h"
 
 namespace clice {
 
@@ -12,9 +11,9 @@ void dbg(const proto::DocumentSymbolResult& result, size_t ident = 0) {
     for(auto& item: result) {
         for(size_t i = 0; i < ident; ++i)
             llvm::outs() << ' ';
-        llvm::outs() << std::format("name: {}, kind:{}, detail:{}, deprecated:{}, children_num:{}",
-                                    item.name,
+        llvm::outs() << std::format("kind: {}, name:{}, detail:{}, deprecated:{}, children_num:{}",
                                     item.kind.name(),
+                                    item.name,
                                     item.detail,
                                     item.deprecated,
                                     item.children.size())
@@ -24,7 +23,20 @@ void dbg(const proto::DocumentSymbolResult& result, size_t ident = 0) {
     }
 }
 
-TEST(DocumentSymbol, NamespaceAndStruct) {
+void total_size(const proto::DocumentSymbolResult& result, size_t& size) {
+    for(auto& item: result) {
+        ++size;
+        total_size(item.children, size);
+    }
+}
+
+size_t total_size(const proto::DocumentSymbolResult& result) {
+    size_t size = 0;
+    total_size(result, size);
+    return size;
+}
+
+TEST(DocumentSymbol, Namespace) {
     const char* main = R"cpp(
 namespace _1 {
     namespace _2 {
@@ -52,7 +64,155 @@ namespace _1::_2{
     auto& info = txs.info;
     proto::DocumentSymbolParams param;
     auto res = feature::documentSymbol(param, info);
-    dbg(res);
+    // dbg(res);
+    ASSERT_EQ(total_size(res), 8);
+}
+
+TEST(DocumentSymbol, Struct) {
+    const char* main = R"cpp(
+struct _1 {};
+struct _2 {};
+
+struct _3 {
+    struct _4 {};
+    struct _5 {};
+};
+
+int main(int argc, char* argv[]) {
+    struct {
+        int x;
+        int y;
+    } point;
+
+    int local = 0; // no symbol for `local` variable
+}
+
+)cpp";
+
+    Tester txs("main.cpp", main);
+    txs.run();
+
+    auto& info = txs.info;
+    proto::DocumentSymbolParams param;
+    auto res = feature::documentSymbol(param, info);
+    // dbg(res);
+    ASSERT_EQ(total_size(res), 9);
+}
+
+TEST(DocumentSymbol, Field) {
+    const char* main = R"cpp(
+
+struct x {
+    int x1;
+    int x2;
+
+    struct y {
+        int y1;
+        int y2;
+    };
+
+    static int x3;
+};
+
+)cpp";
+
+    Tester txs("main.cpp", main);
+    txs.run();
+
+    auto& info = txs.info;
+    proto::DocumentSymbolParams param;
+    auto res = feature::documentSymbol(param, info);
+    // dbg(res);
+    ASSERT_EQ(total_size(res), 7);
+}
+
+TEST(DocumentSymbol, Constructor) {
+    const char* main = R"cpp(
+struct S {
+    int x;
+
+    S(): x(0) {}
+    S(int x) : x(x) {}
+    S(const S& s) : x(s.x) {}
+    ~S() {}
+};
+)cpp";
+
+    Tester txs("main.cpp", main);
+    txs.run();
+
+    auto& info = txs.info;
+    proto::DocumentSymbolParams param;
+    auto res = feature::documentSymbol(param, info);
+    // dbg(res);
+    ASSERT_EQ(total_size(res), 6);
+}
+
+TEST(DocumentSymbol, Method) {
+    const char* main = R"cpp(
+
+struct _0 {
+    void f(int x) {}
+    void f(int* x) {}
+    void f1(int& x) {}
+    void f2(const int& x) {}
+    void f2(const _0& x) {}
+    void f2(_0 x) {}
+};
+
+)cpp";
+
+    Tester txs("main.cpp", main);
+    txs.run();
+
+    auto& info = txs.info;
+    proto::DocumentSymbolParams param;
+    auto res = feature::documentSymbol(param, info);
+    // dbg(res);
+    ASSERT_EQ(total_size(res), 7);
+}
+
+TEST(DocumentSymbol, Enum) {
+    const char* main = R"cpp(
+
+enum class A {
+    _1,
+    _2,
+    _3,
+};
+
+enum B {
+    _a,
+    _b,
+    _c,
+};
+
+)cpp";
+
+    Tester txs("main.cpp", main);
+    txs.run();
+
+    auto& info = txs.info;
+    proto::DocumentSymbolParams param;
+    auto res = feature::documentSymbol(param, info);
+    // dbg(res);
+    ASSERT_EQ(total_size(res), 8);
+}
+
+TEST(DocumentSymbol, TopLevelVariable) {
+    const char* main = R"cpp(
+constexpr auto x = 1;
+int y = 2;
+)cpp";
+
+    Tester txs("main.cpp", main);
+    txs.run();
+
+    auto& info = txs.info;
+    proto::DocumentSymbolParams param;
+    auto res = feature::documentSymbol(param, info);
+    // dbg(res);
+    ASSERT_EQ(total_size(res), 2);
 }
 
 }  // namespace
