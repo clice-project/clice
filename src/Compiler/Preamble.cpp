0f35556cabf30d264c10a928df22f07f72f86afc
ykiko <ykikoykikoykiko@gmail.com>
2025-01-18 14:37:29 +0800

    implement `computePreamble`.

--- src/Compiler/Preamble.cpp
+++ src/Compiler/Preamble.cpp
@@ -10,18 +10,20 @@ std::uint32_t computeBounds(llvm::StringRef content) {
     // Create a lexer starting at the beginning of the file. Note that we use a
     // "fake" file source location at offset 1 so that the lexer will track our
     // position within the file.
-    clang::Lexer lexer(clang::SourceLocation::getFromRawEncoding(1),
-                       langOpts,
-                       content.begin(),
-                       content.begin(),
-                       content.end());
+    auto beginLoc = clang::SourceLocation::getFromRawEncoding(1);
+    clang::Lexer lexer(beginLoc, langOpts, content.begin(), content.begin(), content.end());
 
     bool isInDirective = false;
 
     clang::Token token;
     clang::Token end;
 
-    while(!lexer.LexFromRawLexer(token)) {
+    while(true) {
+        lexer.LexFromRawLexer(token);
+        if(token.is(clang::tok::eof)) {
+            break;
+        }
+
         if(isInDirective) {
             /// If we are in a directive, we should skip the rest of the line.
             if(!token.isAtStartOfLine()) {
@@ -32,13 +34,13 @@ std::uint32_t computeBounds(llvm::StringRef content) {
             }
         }
 
-        if(token.isAtStartOfLine() && token.getKind() == clang::tok::hash) {
+        if(token.isAtStartOfLine() && token.is(clang::tok::hash)) {
             /// If we encounter a `#` at the start of a line, it must be a directive.
             isInDirective = true;
             continue;
-        } else if(token.isAtStartOfLine() && token.getKind() == clang::tok::identifier &&
+        } else if(token.isAtStartOfLine() && token.is(clang::tok::raw_identifier) &&
                   token.getRawIdentifier() == "module") {
-            if(!lexer.LexFromRawLexer(token) && token.getKind() == clang::tok::semi) {
+            if(!lexer.LexFromRawLexer(token) && token.is(clang::tok::semi)) {
                 /// If we encounter a `module` followed by a `;`, it must be
                 /// a global module fragment. It should be a part of the preamble.
                 end = token;
@@ -49,7 +51,11 @@ std::uint32_t computeBounds(llvm::StringRef content) {
         break;
     }
 
-    return end.getLocation().getRawEncoding() + end.getLength();
+    if(auto endLocation = end.getLocation(); endLocation.isValid()) {
+        return endLocation.getRawEncoding() - beginLoc.getRawEncoding() + end.getLength();
+    } else {
+        return 0;
+    }
 }
 
 std::uint32_t computeBounds(CompilationParams& params) {
--- unittests/Compiler/Preamble.cpp
+++ unittests/Compiler/Preamble.cpp
@@ -1,25 +1,51 @@
 #include "Test/Test.h"
 #include "Basic/SourceConverter.h"
 #include "Compiler/Preamble.h"
+#include "Support/Format.h"
 
 namespace clice::testing {
 
 namespace {
 
 TEST(Preamble, ComputeBounds) {
-    const char* normal = R"cpp(
+    proto::Position pos;
+    SourceConverter converter;
+
+    Annotation annotation = {"\n\n\nint x = 1;"};
+    auto compute = [&](llvm::StringRef source) {
+        annotation = {source};
+        auto content = annotation.source();
+        return converter.toPosition(content, computeBounds(content));
+    };
+
+    pos = compute("#include <iostream>$(end)");
+    EXPECT_EQ(pos, annotation.pos("end"));
+
+    pos = compute("#include <iostream>$(end)\n");
+    EXPECT_EQ(pos, annotation.pos("end"));
+
+    pos = compute(R"cpp(
 #ifdef TEST
 #include <iostream>
 #define 1
-#endif
-    )cpp";
+#endif$(end)
+    )cpp");
+    EXPECT_EQ(pos, annotation.pos("end"));
 
-    SourceConverter converter;
+    pos = compute(R"cpp(
+#include <iostream>$(end)
+int x = 1;
+    )cpp");
+    EXPECT_EQ(pos, annotation.pos("end"));
 
-    llvm::outs() << computeBounds(normal) << '\n';
+    pos = compute(R"cpp(
+module;
+#include <iostream>$(end)
+export module test;
+    )cpp");
+    EXPECT_EQ(pos, annotation.pos("end"));
 }
 
 }  // namespace
-
 }  // namespace clice::testing
 
