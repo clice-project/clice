a9dce8976e3c45d8a0844a3c551040de5dde4d2b
ykiko <ykikoykikoykiko@gmail.com>
2025-02-09 13:52:42 +0800

    Clear `async` tests.

--- unittests/Async/Task.cpp
+++ unittests/Async/Task.cpp
@@ -5,7 +5,64 @@ namespace clice::testing {
 
 namespace {
 
-TEST(Async, Cancel) {
+TEST(Async, TaskAwait) {
+    static auto my_task1 = []() -> async::Task<int> {
+        co_return 1;
+    };
+
+    static auto my_task2 = []() -> async::Task<int> {
+        auto result = co_await my_task1();
+        co_return result + 1;
+    };
+
+    static auto my_task3 = []() -> async::Task<int> {
+        auto result = co_await my_task2();
+        co_return result + 1;
+    };
+
+    auto [result] = async::run(my_task3());
+    EXPECT_EQ(result, 3);
+}
+
+TEST(Async, TaskDispose) {
+    static int x = 1;
+
+    struct X {
+        ~X() {
+            x += 1;
+        }
+    };
+
+    auto my_task = [&]() -> async::Task<> {
+        X x;
+        co_await async::sleep(std::chrono::milliseconds(500));
+    };
+
+    auto task = my_task();
+    task.schedule();
+    task.dispose();
+
+    async::run();
+
+    EXPECT_EQ(x, 2);
+
+    auto main = [&]() -> async::Task<> {
+        auto task = my_task();
+        task.schedule();
+        co_await async::sleep(std::chrono::milliseconds(100));
+        task.cancel();
+        task.dispose();
+    };
+
+    auto p = main();
+    p.schedule();
+
+    async::run();
+
+    EXPECT_EQ(x, 3);
+}
+
+TEST(Async, TaskCancel) {
     int x = 1;
 
     auto my_task = [&]() -> async::Task<> {
