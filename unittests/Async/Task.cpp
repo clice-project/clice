3305119ad86bdd7ab3423f250aebe48f09325b1e
ykiko <ykikoykikoykiko@gmail.com>
2025-02-12 00:35:38 +0800

    Enable `Task::cancel` tests.

--- unittests/Async/Task.cpp
+++ unittests/Async/Task.cpp
@@ -63,28 +63,28 @@ TEST(Async, TaskDispose) {
 }
 
 TEST(Async, TaskCancel) {
-    // int x = 1;
-    //
-    // auto my_task = [&]() -> async::Task<> {
-    //    x = 2;
-    //    co_await async::sleep(std::chrono::milliseconds(300));
-    //    x = 3;
-    //};
-    //
-    // auto main = [&]() -> async::Task<> {
-    //    auto task = my_task();
-    //    task.schedule();
-    //    co_await async::sleep(std::chrono::milliseconds(100));
-    //    task.cancel();
-    //    task.dispose();
-    //};
-    //
-    // auto p = main();
-    // p.schedule();
-    //
-    // async::run();
-    //
-    // EXPECT_EQ(x, 2);
+    int x = 1;
+
+    auto my_task = [&]() -> async::Task<> {
+        x = 2;
+        co_await async::sleep(std::chrono::milliseconds(300));
+        x = 3;
+    };
+
+    auto main = [&]() -> async::Task<> {
+        auto task = my_task();
+        task.schedule();
+        co_await async::sleep(std::chrono::milliseconds(100));
+        task.cancel();
+        task.dispose();
+    };
+
+    auto p = main();
+    p.schedule();
+
+    async::run();
+
+    EXPECT_EQ(x, 2);
 }
 
 }  // namespace
