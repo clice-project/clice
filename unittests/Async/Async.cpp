f7dca3e76b9fb334034defc910dee920c370ab4b
ykiko <ykikoykikoykiko@gmail.com>
2025-02-12 13:40:49 +0800

    Add `async::Event`.

--- unittests/Async/Async.cpp
+++ unittests/Async/Async.cpp
@@ -4,34 +4,7 @@
 
 namespace clice::testing {
 
-namespace {
-
-TEST(Async, Submit) {
-    using result = async::Task<std::expected<std::thread::id, std::error_code>>;
-
-    auto task = []() -> result {
-        co_return co_await async::submit([]() {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            return std::this_thread::get_id();
-        });
-    }();
-
-    auto task2 = []() -> result {
-        co_return co_await async::submit([]() {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            return std::this_thread::get_id();
-        });
-    }();
-
-    auto task3 = []() -> result {
-        co_return co_await async::submit([]() {
-            std::this_thread::sleep_for(std::chrono::seconds(1));
-            return std::this_thread::get_id();
-        });
-    }();
-}
-
-}  // namespace
+namespace {}  // namespace
 
 }  // namespace clice::testing
 
