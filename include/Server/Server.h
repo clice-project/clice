c622a1ceba69534ffcbcf99e07f281e23518e86c
ykiko <ykikoykikoykiko@gmail.com>
2025-02-23 02:06:53 +0800

    Some update.

--- include/Server/Server.h
+++ include/Server/Server.h
@@ -3,6 +3,7 @@
 #include "Config.h"
 #include "Indexer.h"
 #include "Protocol.h"
+#include "LSPConverter.h"
 
 #include "Async/Async.h"
 #include "Compiler/Command.h"
@@ -15,6 +16,7 @@ public:
 
     async::Task<> onReceive(json::Value value);
 
+private:
     /// Send a request to the client.
     async::Task<> request(llvm::StringRef method, json::Value params);
 
@@ -28,113 +30,13 @@ public:
     async::Task<> registerCapacity(llvm::StringRef id,
                                    llvm::StringRef method,
                                    json::Value registerOptions);
+
+    async::Task<> initialize(const proto::InitializedParams& params);
+
+public:
     std::uint32_t id = 0;
-
-private:
-    /// ============================================================================
-    ///                            Lifecycle Message
-    /// ============================================================================
-
-    /// async::Task<> onInitialize(json::Value id, const proto::InitializeParams& params);
-
-    /// async::Task<> onInitialized(const proto::InitializedParams& params);
-
-    /// async::Task<> onShutdown(json::Value id, const proto::None&);
-
-    /// async::Task<> onExit(const proto::None&);
-
-    /// ============================================================================
-    ///                         Document Synchronization
-    /// ============================================================================
-
-    /// async::Task<> onDidOpen(const proto::DidOpenTextDocumentParams& document);
-
-    /// async::Task<> onDidChange(const proto::DidChangeTextDocumentParams& document);
-
-    /// async::Task<> onDidSave(const proto::DidSaveTextDocumentParams& document);
-
-    /// async::Task<> onDidClose(const proto::DidCloseTextDocumentParams& document);
-
-    /// ============================================================================
-    ///                             Language Features
-    /// ============================================================================
-
-    // async::Task<> onGotoDeclaration(json::Value id, const proto::DeclarationParams& params);
-    //
-    // async::Task<> onGotoDefinition(json::Value id, const proto::DefinitionParams& params);
-    //
-    // async::Task<> onGotoTypeDefinition(json::Value id, const proto::TypeDefinitionParams&
-    // params);
-    //
-    // async::Task<> onGotoImplementation(json::Value id, const proto::ImplementationParams&
-    // params);
-    //
-    // async::Task<> onFindReferences(json::Value id, const proto::ReferenceParams& params);
-    //
-    // async::Task<> onPrepareCallHierarchy(json::Value id,
-    //                                      const proto::CallHierarchyPrepareParams& params);
-    //
-    // async::Task<> onIncomingCall(json::Value id,
-    //                              const proto::CallHierarchyIncomingCallsParams& params);
-    //
-    // async::Task<> onOutgoingCall(json::Value id,
-    //                              const proto::CallHierarchyOutgoingCallsParams& params);
-    //
-    // async::Task<> onPrepareTypeHierarchy(json::Value id,
-    //                                      const proto::TypeHierarchyPrepareParams& params);
-    //
-    // async::Task<> onSupertypes(json::Value id, const proto::TypeHierarchySupertypesParams&
-    // params);
-    //
-    // async::Task<> onSubtypes(json::Value id, const proto::TypeHierarchySubtypesParams& params);
-
-    // async::Task<> onDocumentHighlight(json::Value id, const proto::DocumentHighlightParams&
-    // params);
-    //
-    // async::Task<> onDocumentLink(json::Value id, const proto::DocumentLinkParams& params);
-    //
-    // async::Task<> onHover(json::Value id, const proto::HoverParams& params);
-    //
-    // async::Task<> onCodeLens(json::Value id, const proto::CodeLensParams& params);
-    //
-    // async::Task<> onFoldingRange(json::Value id, const proto::FoldingRangeParams& params);
-    //
-    // async::Task<> onDocumentSymbol(json::Value id, const proto::DocumentSymbolParams& params);
-    //
-    // async::Task<> onSemanticTokens(json::Value id, const proto::SemanticTokensParams& params);
-    //
-    // async::Task<> onInlayHint(json::Value id, const proto::InlayHintParams& params);
-    //
-    // async::Task<> onCodeCompletion(json::Value id, const proto::CompletionParams& params);
-    //
-    // async::Task<> onSignatureHelp(json::Value id, const proto::SignatureHelpParams& params);
-    //
-    // async::Task<> onCodeAction(json::Value id, const proto::CodeActionParams& params);
-    //
-    // async::Task<> onFormatting(json::Value id, const proto::DocumentFormattingParams& params);
-    //
-    // async::Task<> onRangeFormatting(json::Value id,
-    //                                const proto::DocumentRangeFormattingParams& params);
-
-    /// ============================================================================
-    ///                             Workspace Features
-    /// ============================================================================
-
-    /// async::Task<> onDidChangeWatchedFiles(const proto::DidChangeWatchedFilesParams& params);
-
-    /// ============================================================================
-    ///                                 Extension
-    /// ============================================================================
-
-    /// async::Task<> onIndexCurrent(const proto::TextDocumentIdentifier& params);
-
-    /// async::Task<> onIndexAll(const proto::None&);
-
-    /// async::Task<> onContextCurrent(const proto::TextDocumentIdentifier& params);
-
-    /// async::Task<> onContextAll(const proto::TextDocumentIdentifier& params);
-
-    /// async::Task<> onContextSwitch(const proto::TextDocumentIdentifier& params);
+    // Indexer indexer;
+    // LSPConverter& converter;
 
     /// SourceConverter converter;
     /// CompilationDatabase database;
