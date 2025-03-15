e63303703bbb4948a77a2e222e9532efa15a2e3d
ykiko <ykikoykikoykiko@gmail.com>
2025-03-15 20:08:26 +0800

    Clear `CodeCompletion` and `SignatureHelp`.

--- include/Server/Scheduler.h
+++ include/Server/Scheduler.h
@@ -2,16 +2,31 @@
 
 #include "Async/Async.h"
 #include "Compiler/Command.h"
+#include "Feature/CodeCompletion.h"
+#include "Feature/SignatureHelp.h"
 
 namespace clice {
 
 class ASTInfo;
 
-
-
 class Scheduler {
 public:
-    async::Task<ASTInfo> build(llvm::StringRef tu);
+    /// Build the given source file with given content. If there is not
+    /// preamble for it, build the preamble first.
+    async::Task<ASTInfo> build(llvm::StringRef file, llvm::StringRef content);
+
+    /// Build the given source directly without preamble.
+    async::Task<ASTInfo> build(llvm::StringRef file);
+
+    async::Task<feature::CodeCompletionResult> codeCompletion(llvm::StringRef file,
+                                                              llvm::StringRef content,
+                                                              std::uint32_t line,
+                                                              std::uint32_t column);
+
+    async::Task<feature::SignatureHelpResult> signatureHelp(llvm::StringRef file,
+                                                            llvm::StringRef content,
+                                                            std::uint32_t line,
+                                                            std::uint32_t column);
 
 private:
     CompilationDatabase database;
