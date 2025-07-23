0ce30fb072b19066d1b0e8a8943f251ef8b140b4
ykiko <ykikoykikoykiko@gmail.com>
2025-07-23 10:05:07 +0800

    Add `DidOpen` test (#160)

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,8 +13,24 @@ def pytest_addoption(parser):
 
 @pytest.fixture(scope="session")
 def executable(request):
-    path = request.config.getoption("--executable")
-    return Path(path).resolve()
+    executable = request.config.getoption("--executable")
+    if executable is None:
+        pytest.exit(
+            "Error: You must specify the 'clice' executable path using "
+            "'--executable=<path/to/clice>' in pytest arguments, "
+            "or configure it in your pytest.ini/conftest.py.",
+            returncode=64
+        )
+
+    path = Path(executable)
+    if not path.exists():
+        pytest.exit(
+            f"Error: 'clice' executable not found at '{executable}'. "
+            "Please ensure the path is correct and the file exists.",
+            returncode=64
+        )
+
+    return path.resolve()
 
 
 @pytest.fixture(scope="session")
--- tests/fixtures/client.py
+++ tests/fixtures/client.py
@@ -5,6 +5,8 @@ from .transport import LSPTransport
 class LSPClient(LSPTransport):
     def __init__(self, commands, mode="stdio", host="127.0.0.1", port=2087):
         super().__init__(commands, mode, host, port)
+        self.workspace = ""
+        self.opening_files: dict[Path, str] = {}
 
     async def initialize(self, workspace: str):
         self.workspace = workspace
@@ -18,3 +20,29 @@ class LSPClient(LSPTransport):
     async def exit(self):
         await self.send_notification("exit")
         await self.stop()
+
+    def get_abs_path(self, relative_path):
+        return Path(self.workspace, relative_path)
+
+    async def did_open(self, relative_path: str):
+        path = self.get_abs_path(relative_path)
+
+        content = ""
+        with open(path, encoding="utf-8") as file:
+            content = file.read()
+
+        if path in self.opening_files:
+            raise f"Cannot open same file multiple times: {path}"
+
+        self.opening_files[path] = content
+
+        params = {
+            "textDocument": {
+                "uri": path.as_uri(),
+                "languageId": "cpp",
+                "version": 0,
+                "text": content,
+            }
+        }
+
+        await self.send_notification("textDocument/didOpen", params)
