a57b65137adf3ce9b451a1b81c0917830dc16d1f
ykiko <ykikoykikoykiko@gmail.com>
2025-08-09 12:49:21 +0800

    Check PCH update.

--- src/Server/Document.cpp
+++ src/Server/Document.cpp
@@ -5,37 +5,46 @@
 
 namespace clice {
 
-async::Task<> Server::build_pch(std::string path, std::string content) {
+async::Task<> Server::build_pch(std::string file, std::string content) {
     auto bound = compute_preamble_bound(content);
 
-    auto open_file = &opening_files[path];
+    auto open_file = &opening_files[file];
+    auto info = database.get_command(file, true, true);
 
-    /// Check update ...
-    if(open_file->pch) {
-        bool outdated = false;
+    auto check_pch_update = [&content, &bound, &info](PCHInfo& pch) {
+        if(content.substr(0, bound) != pch.preamble) {
+            return true;
+        }
 
-        auto& info = *open_file->pch;
-        for(auto& dep: info.deps) {
+        if(info.arguments != pch.arguments) {
+            return true;
+        }
+
+        /// Check deps.
+        for(auto& dep: pch.deps) {
             fs::file_status status;
             auto error = fs::status(dep, status, true);
             if(error || std::chrono::duration_cast<std::chrono::milliseconds>(
                             status.getLastModificationTime().time_since_epoch())
-                                .count() > info.mtime) {
-                outdated = true;
-                break;
+                                .count() > pch.mtime) {
+                return true;
             }
         }
 
+        return false;
+    };
+
+    /// Check update ...
+    if(open_file->pch && !check_pch_update(*open_file->pch)) {
         /// If not need update, return directly.
-        if(!outdated) {
-            log::info("PCH is already up-to-date for {}", path);
-            co_return;
-        }
+        log::info("PCH is already up-to-date for {}", file);
+        co_return;
     }
 
     /// The actual PCH build task.
     constexpr static auto PCHBuildTask =
-        [](Server& server,
+        [](CompilationDatabase::LookupInfo& info,
+           OpenFile* open_file,
            std::string path,
            std::uint32_t bound,
            std::string content,
@@ -53,11 +62,11 @@ async::Task<> Server::build_pch(std::string path, std::string content) {
 
         CompilationParams params;
         params.output_file = path::join(config::cache.dir, path::filename(path) + ".pch");
-        params.arguments = server.database.get_command(path, true, true).arguments;
+        params.arguments = std::move(info.arguments);
         params.diagnostics = diagnostics;
         params.add_remapped_file(path, content, bound);
 
-        PCHInfo info;
+        PCHInfo pch;
 
         std::string command;
         for(auto argument: params.arguments) {
@@ -67,45 +76,43 @@ async::Task<> Server::build_pch(std::string path, std::string content) {
 
         log::info("Start building PCH for {}, command: [{}]", path, command);
 
+        std::string message;
         std::vector<feature::DocumentLink> links;
 
-        /// PCH file is written until destructing, Add a single block
-        /// for it.
-        bool cond = co_await async::submit([&] {
-            auto result = compile(params, info);
-            if(!result) {
-                log::warn("Building PCH fails for {}, Because: {}", path, result.error());
-
-                for(auto& diagnostic: *diagnostics) {
-                    log::warn("{}", diagnostic.message);
-                }
+        bool success = co_await async::submit([&params, &pch, &message, &links] -> bool {
+            /// PCH file is written until destructing, Add a single block
+            /// for it.
+            auto unit = compile(params, pch);
+            if(!unit) {
+                message = std::move(unit.error());
                 return false;
             }
 
-            links = feature::document_links(*result);
-
-            /// TODO: index PCH.
-
+            links = feature::document_links(*unit);
+            /// TODO: index PCH file, etc
             return true;
         });
 
-        if(!cond) {
+        if(!success) {
+            log::warn("Building PCH fails for {}, Because: {}", path, message);
+            for(auto& diagnostic: *diagnostics) {
+                log::warn("{}", diagnostic.message);
+            }
             co_return false;
         }
 
-        auto& openFile = server.opening_files[path];
         /// Update the built PCH info.
-        openFile.pch = std::move(info);
-        openFile.pch_includes = std::move(links);
+        open_file->pch = std::move(pch);
+        open_file->pch_includes = std::move(links);
 
         /// Resume waiters on this event.
-        openFile.pch_built_event.set();
-        openFile.pch_built_event.clear();
+        open_file->pch_built_event.set();
+        open_file->pch_built_event.clear();
 
         co_return true;
     };
 
-    open_file = &opening_files[path];
+    open_file = &opening_files[file];
 
     /// If there is already an PCH build task, cancel it.
     auto& task = open_file->pch_build_task;
@@ -115,10 +122,10 @@ async::Task<> Server::build_pch(std::string path, std::string content) {
     }
 
     /// Schedule the new building task.
-    task = PCHBuildTask(*this, path, bound, std::move(content), open_file->diagnostics);
+    task = PCHBuildTask(info, open_file, file, bound, std::move(content), open_file->diagnostics);
 
     if(co_await task) {
-        log::info("Building PCH successfully for {}", path);
+        log::info("Building PCH successfully for {}", file);
 
         /// Dispose the task so that it will destroyed when task complete.
         task.dispose();
