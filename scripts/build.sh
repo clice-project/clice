60cd9f11d51e43b88cddd6f34e5472b671e7123d
Myriad-Dreamin <camiyoru@gmail.com>
2025-08-25 18:39:07 +0000

    Update build script to use env shebang and enable parallel builds

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # Online Documentation: https://clice.io/dev/build.html
 # This is a sample script to build the project.
@@ -19,4 +19,4 @@ BUILD_DIR="build"
 COMPILER_FLAGS="-DCMAKE_C_COMPILER=clang -DCMAKE_CXX_COMPILER=clang++"
 
 cmake -B "$BUILD_DIR" -G Ninja -DLLVM_INSTALL_PATH="$LLVM_INSTALL_PATH" $COMPILER_FLAGS -DCMAKE_BUILD_TYPE=Debug -DCLICE_ENABLE_TEST=ON
-cmake --build "$BUILD_DIR"
+cmake --build "$BUILD_DIR" -j
