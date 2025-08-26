588f15c67026a3b04168e631c246314b5f7d28a9
Myriad-Dreamin <camiyoru@gmail.com>
2025-08-26 03:43:16 +0000

    Add additional unit tests for `isFastTidyCheck` in Tidy.cpp

--- tests/unit/Server/Tidy.cpp
+++ tests/unit/Server/Tidy.cpp
@@ -9,6 +9,11 @@ suite<"ClangTidy"> clang_tidy = [] {
     test("isFastTidyCheck") = [] {
         expect(that % tidy::isFastTidyCheck("readability-misleading-indentation"));
         expect(that % tidy::isFastTidyCheck("bugprone-unused-return-value"));
+
+        // clangd/unittests/TidyProviderTests.cpp
+        expect(that % tidy::isFastTidyCheck("misc-const-correctness") == false);
+        expect(that % tidy::isFastTidyCheck("bugprone-suspicious-include") == true);
+        expect(that % tidy::isFastTidyCheck("replay-preamble-check") == std::nullopt);
     };
 };
 
