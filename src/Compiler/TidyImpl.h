42a3c7da30a855a389cc461fdf614772e0644e30
Myriad-Dreamin <camiyoru@gmail.com>
2025-08-26 03:40:22 +0000

    Add implementations from clangd to Tidy.cpp
    Add clang-tidy support in CompilationParams
    
    Remove the legacy fast check filter option

---
+++ src/Compiler/TidyImpl.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "clang-tidy/ClangTidyModuleRegistry.h"
+#include "clang-tidy/ClangTidyOptions.h"
+#include "clang-tidy/ClangTidyCheck.h"
+
+#include "Compiler/Diagnostic.h"
+#include "Compiler/Tidy.h"
+
+namespace clice::tidy {
+
+using namespace clang::tidy;
+
+class ClangTidyChecker {
+    /// Create instances of checks that are enabled for the current Language.
+    std::vector<std::unique_ptr<ClangTidyCheck>> checks;
+
+public:
+    ClangTidyContext context;
+    clang::ast_matchers::MatchFinder CTFinder;
+
+    ClangTidyChecker(std::unique_ptr<ClangTidyOptionsProvider> provider);
+
+    clang::DiagnosticsEngine::Level adjustLevel(clang::DiagnosticsEngine::Level DiagLevel,
+                                                const clang::Diagnostic& Info);
+
+    void adjustDiag(Diagnostic& Diag);
+};
+
+}  // namespace clice::tidy
