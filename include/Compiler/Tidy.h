451db04162da7d4eafc51d38c1497aa6e68e3388
Myriad-Dreamin <camiyoru@gmail.com>
2025-08-26 03:40:22 +0000

    Add implementations from clangd to Tidy.cpp
    Add clang-tidy support in CompilationParams
    
    Remove the legacy fast check filter option

--- include/Compiler/Tidy.h
+++ include/Compiler/Tidy.h
@@ -2,9 +2,23 @@
 
 #include "llvm/ADT/StringRef.h"
 
+#include <memory>
+
+namespace clang {
+class CompilerInstance;
+}
+
 namespace clice::tidy {
 
 bool isRegisteredTidyCheck(llvm::StringRef check);
 std::optional<bool> isFastTidyCheck(llvm::StringRef check);
+
+struct TidyParams {};
+
+class ClangTidyChecker;
+
+/// Run clang-tidy on the given file.
+std::unique_ptr<ClangTidyChecker> configure(clang::CompilerInstance& instance,
+                                            const TidyParams& params);
 
 }  // namespace clice::tidy
