ab77c651cf09e7f843bbee4e1e2f0dbc8a398418
Myriad-Dreamin <camiyoru@gmail.com>
2025-09-08 02:53:16 +0000

    Fix destructor logic in CompilationUnit to properly handle instance and action cleanup

--- src/Compiler/CompilationUnit.cpp
+++ src/Compiler/CompilationUnit.cpp
@@ -5,12 +5,14 @@
 namespace clice {
 
 CompilationUnit::~CompilationUnit() {
-    if(impl && impl->action) {
+    if(impl && impl->instance) {
         auto instance = impl->instance.get();
         // We already notified the PP of end-of-file earlier, so detach it first.
         // We must keep it alive until after EndSourceFile(), Sema relies on this.
         auto PP = instance->getPreprocessorPtr();
         instance->setPreprocessor(nullptr);  // Detach so we don't send EOF again.
+    }
+    if(impl && impl->action) {
         impl->action->EndSourceFile();
     }
 
